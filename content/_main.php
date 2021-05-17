b7a90fbde46d6b03693663a7c600c28d6d8f41a4
anniks2010 <anulember@hot.ee>
2021-05-18 00:37:08 +0300

    img change

--- content/_main.php
+++ content/_main.php
@@ -1,5 +1,7 @@
 <div class="container">
+    <div class="main_img">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIHz0Oj1TqZEq-RzYBGpJQDVmrEL6nSL2AFQ&usqp=CAU" alt="welcome" style="width:900px;height:300px;border:0px;">
+</div>
     <p>This website is all about modern-day technologies for both low skilled and advanced users. Here we offer the most interesting computer-related information from all around the world.</p>
     <p>
     Here you can also look up PC builds that suit specifically your needs. If you don't  know which pc part fits your build the most, fea not, on our webiste you can compare parts and make a weighed decision about what to pick.
