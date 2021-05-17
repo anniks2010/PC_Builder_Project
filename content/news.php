f63d4772a6fd9e55bb56d494c36390db5fd77681
Katlintthk <pajukatlin@gmail.com>
2021-05-17 22:13:51 +0300

    updated news

--- content/news.php
+++ content/news.php
@@ -1,6 +1,6 @@
 <?php
-    require_once __DIR__ . "/database.php";
-    global $conn;
+   /* require_once __DIR__ . "/database.php";
+    global $conn; */
 
 ?>
 
@@ -15,7 +15,7 @@
 
 <body>
 <header>
-    <div class="logo">
+    <div class="">
         PC BUILDER PROJECT
     </div>
     <nav>
@@ -28,62 +28,62 @@
 
         </ul>
     </nav>
-    <div class="clear"></div>
+    <div class="clear_news"></div>
 </header>
 
-<div class="container">
+<div class="container_news">
     <h1>News</h1>
 
 
 <div class="">
-    <div class="row">
-        <div class="picture">
+    <div class="row_news">
+        <div class="picture_news">
             <a href="">
-                <img src="http://s4.thingpic.com/images/CM/uHYVBitbGD8jGxsGKoa74rtn.jpeg" alt="PILT REBANE">
+                <img class ="img_news" src="http://s4.thingpic.com/images/CM/uHYVBitbGD8jGxsGKoa74rtn.jpeg" alt="PILT REBANE">
             </a>      
         </div>
-        <div class="intro">
+        <div class="intro_news">
             <h2 class="intro_link" style="color: #4b8d89; margin-top: -2px;">
                 <a class="intro_link" href="">TEST UUDIS</a>
             </h2>
             <p style="margin-top: 5px;margin-bottom: 5px;">veebruar 23, 2021</p>
             <p>Proin id dui molestie, vehicula augue et, tincidunt dolor. Fusce suscipit pharetra nulla eu sollicitudin. Nulla mollis varius congue. Sed justo eros, blandit in fringilla eu, vestibulum sit amet felis. Donec ut massa cursus risus convallis tempor eu eu arcu. Etiam auctor leo sem, vitae tristique nisl fringilla sit amet. Phasellus eu laoreet dolor, in faucibus sapien. Nullam id dui ut nisl tincidunt rutrum id interdum dui. Sed vestibulum tempor ipsum. Sed scelerisque urna quis venenatis aliquam. Morbi congue tempor lorem eu tempus. Nam commodo luctus neque id euismod.</p>
         </div>
-        <div class="clear"></div>
+        <div class="clear_news"></div>
     </div>
 
 
-    <div class="row">
-        <div class="picture">
+    <div class="row_news">
+        <div class="picture_news">
             <a href="">
                 <img src="http://s4.thingpic.com/images/CM/uHYVBitbGD8jGxsGKoa74rtn.jpeg" alt="PILT REBANE">
             </a>      
         </div>
-        <div class="intro">
+        <div class="intro_news">
             <h2 class="intro_link" style="color: #4b8d89; margin-top: -2px;">
                 <a class="intro_link" href="">TEST UUDIS</a>
             </h2>
             <p style="margin-top: 5px;margin-bottom: 5px;">veebruar 23, 2021</p>
             <p>Proin id dui molestie, vehicula augue et, tincidunt dolor. Fusce suscipit pharetra nulla eu sollicitudin. Nulla mollis varius congue. Sed justo eros, blandit in fringilla eu, vestibulum sit amet felis. Donec ut massa cursus risus convallis tempor eu eu arcu. Etiam auctor leo sem, vitae tristique nisl fringilla sit amet. Phasellus eu laoreet dolor, in faucibus sapien. Nullam id dui ut nisl tincidunt rutrum id interdum dui. Sed vestibulum tempor ipsum. Sed scelerisque urna quis venenatis aliquam. Morbi congue tempor lorem eu tempus. Nam commodo luctus neque id euismod.</p>
         </div>
-        <div class="clear"></div>
+        <div class="clear_news"></div>
     </div>
 
 
-    <div class="row">
-        <div class="picture">
+    <div class="row_news">
+        <div class="picture_news">
             <a href="">
                 <img src="http://s4.thingpic.com/images/CM/uHYVBitbGD8jGxsGKoa74rtn.jpeg" alt="PILT REBANE">
             </a>      
         </div>
-        <div class="intro">
+        <div class="intro_news">
             <h2 class="intro_link" style="color: #4b8d89; margin-top: -2px;">
                 <a class="intro_link" href="">TEST UUDIS</a>
             </h2>
-            <p>veebruar 23, 2021</p>
+            <p style="margin-top: 5px;margin-bottom: 5px;">veebruar 23, 2021</p>
             <p>Proin id dui molestie, vehicula augue et, tincidunt dolor. Fusce suscipit pharetra nulla eu sollicitudin. Nulla mollis varius congue. Sed justo eros, blandit in fringilla eu, vestibulum sit amet felis. Donec ut massa cursus risus convallis tempor eu eu arcu. Etiam auctor leo sem, vitae tristique nisl fringilla sit amet. Phasellus eu laoreet dolor, in faucibus sapien. Nullam id dui ut nisl tincidunt rutrum id interdum dui. Sed vestibulum tempor ipsum. Sed scelerisque urna quis venenatis aliquam. Morbi congue tempor lorem eu tempus. Nam commodo luctus neque id euismod.</p>
         </div>
-        <div class="clear"></div>
+        <div class="clear_news"></div>
     </div>
 
 </div>
