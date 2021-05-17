45ea0161bff9a958198c3922f41b19078f38eceb
Katlintthk <pajukatlin@gmail.com>
2021-05-17 22:39:06 +0300

    news

--- content/news.php
+++ content/news.php
@@ -60,39 +60,6 @@
     </div>
 
 
-    <div class="row_news">
-        <div class="picture_news">
-            <a href="">
-                <img class ="img_news" src="http://s4.thingpic.com/images/CM/uHYVBitbGD8jGxsGKoa74rtn.jpeg" alt="PILT REBANE">
-            </a>      
-        </div>
-        <div class="intro_news">
-            <h2 class="intro_link" style="color: #4b8d89; margin-top: -2px;">
-                <a class="intro_link" href="">TEST UUDIS</a>
-            </h2>
-            <p style="margin-top: 5px;margin-bottom: 5px;">veebruar 23, 2021</p>
-            <p>Proin id dui molestie, vehicula augue et, tincidunt dolor. Fusce suscipit pharetra nulla eu sollicitudin. Nulla mollis varius congue. Sed justo eros, blandit in fringilla eu, vestibulum sit amet felis. Donec ut massa cursus risus convallis tempor eu eu arcu. Etiam auctor leo sem, vitae tristique nisl fringilla sit amet. Phasellus eu laoreet dolor, in faucibus sapien. Nullam id dui ut nisl tincidunt rutrum id interdum dui. Sed vestibulum tempor ipsum. Sed scelerisque urna quis venenatis aliquam. Morbi congue tempor lorem eu tempus. Nam commodo luctus neque id euismod.</p>
-        </div>
-        <div class="clear_news"></div>
-    </div>
-
-
-    <div class="row_news">
-        <div class="picture_news">
-            <a href="">
-                <img class ="img_news" src="http://s4.thingpic.com/images/CM/uHYVBitbGD8jGxsGKoa74rtn.jpeg" alt="PILT REBANE">
-            </a>      
-        </div>
-        <div class="intro_news">
-            <h2 class="intro_link" style="color: #4b8d89; margin-top: -2px;">
-                <a class="intro_link" href="">TEST UUDIS</a>
-            </h2>
-            <p style="margin-top: 5px;margin-bottom: 5px;">veebruar 23, 2021</p>
-            <p>Proin id dui molestie, vehicula augue et, tincidunt dolor. Fusce suscipit pharetra nulla eu sollicitudin. Nulla mollis varius congue. Sed justo eros, blandit in fringilla eu, vestibulum sit amet felis. Donec ut massa cursus risus convallis tempor eu eu arcu. Etiam auctor leo sem, vitae tristique nisl fringilla sit amet. Phasellus eu laoreet dolor, in faucibus sapien. Nullam id dui ut nisl tincidunt rutrum id interdum dui. Sed vestibulum tempor ipsum. Sed scelerisque urna quis venenatis aliquam. Morbi congue tempor lorem eu tempus. Nam commodo luctus neque id euismod.</p>
-        </div>
-        <div class="clear_news"></div>
-    </div>
-
 </div>
 
 
