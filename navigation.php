06ce059e02c382a0fadcb96067507f4c9187e030
Katlintthk <pajukatlin@gmail.com>
2021-05-17 22:26:57 +0300

    updated nav

--- navigation.php
+++ navigation.php
@@ -5,5 +5,6 @@
         <li><a href="?leht=builds">Builds</a></li>
         <li><a href="?leht=shop">Shop</a></li>
         <li><a href="?leht=compare">Compare</a></li>
+        <li><a href="?leht=kontakt">Contact</a></li>
     </ul>
 </nav>
