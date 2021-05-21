f6c50cf3a358d4bc3742309a45a6cafc886b00f0
anniks2010 <anulember@hot.ee>
2021-05-21 22:50:39 +0300

    added database file

---
+++ content/database.php
@@ -0,0 +1,13 @@
+<?php
+$host = "d85345.mysql.zonevs.eu"; /* Host name */
+$user = "d85345sa355854"; /* User */
+$password = "BXR9GpAN67h6h4h6dn"; /* Password */
+$dbname = "d85345sd396387"; /* Database name */
+
+$con = mysqli_connect($host, $user, $password, $dbname);
+$con->set_charset("utf8");
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
