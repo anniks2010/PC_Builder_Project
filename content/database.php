733fecfda63cab4d299cc02fa99f69a75ab1ab14
anniks2010 <anulember@hot.ee>
2021-05-26 11:09:25 +0300

    database file update

--- content/database.php
+++ content/database.php
@@ -1,10 +1,9 @@
 <?php
+$host = "d85345.mysql.zonevs.eu"; /* Host name */
+$user = "d85345sa355854"; /* User */
+$password = "BXR9GpAN67h6h4h6dn"; /* Password */
+$dbname = "d85345sd396387"; /* Database name */
 
-$host = "localhost"; /* Host name */
-$user = "pcbuilder"; /* User */
-$password = "1234"; /* Password */
-$dbname = "pcbuilder"; /* Database name */
-global $con;
 $con = mysqli_connect($host, $user, $password, $dbname);
 $con->set_charset("utf8");
 // Check connection
